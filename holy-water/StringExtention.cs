ba62e3d396641fe365f5cc0a070ea10155347710
jonasdan <31690134+jonasdan@users.noreply.github.com>
2017-11-17 19:25:01 +0200

    Added an exception in stringextention (#38)

--- holy-water/StringExtention.cs
+++ holy-water/StringExtention.cs
@@ -5,8 +5,18 @@ namespace holy_water
     public static class StringExtention
     {
         public static decimal ToDecimal(this string str)
-        {
-            return Convert.ToDecimal(str);
+        {   
+
+            try
+            {
+                return Convert.ToDecimal(str);
+            }
+            catch (OverflowException e)
+            {
+                MessageBox.Show("Some of the data has been shortened because it is too long " + e);
+                int lengthMax = (Decimal.MaxValue).ToString().Length;
+                return Convert.ToDecimal(str.Substring(0, Math.Min(str.Length, lengthMax)));
+            }
         }
     }
 }
