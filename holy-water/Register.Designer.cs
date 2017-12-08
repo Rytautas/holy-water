22acc6e775bc0e88b2631b7e8db66becc97658ab
neringach <32042609+neringach@users.noreply.github.com>
2017-12-09 00:35:34 +0200

    New changes (#41)
    * created database, added event
    
    * tread added and other changes
    
    * moved database to Azure

--- holy-water/Register.Designer.cs
+++ holy-water/Register.Designer.cs
@@ -43,18 +43,20 @@
             // checkBox18
             // 
             this.checkBox18.AutoSize = true;
-            this.checkBox18.Location = new System.Drawing.Point(178, 257);
+            this.checkBox18.Location = new System.Drawing.Point(134, 209);
+            this.checkBox18.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox18.Name = "checkBox18";
-            this.checkBox18.Size = new System.Drawing.Size(156, 21);
+            this.checkBox18.Size = new System.Drawing.Size(122, 17);
             this.checkBox18.TabIndex = 4;
             this.checkBox18.Text = "\"I am +18 years old\"";
             this.checkBox18.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(388, 296);
+            this.button1.Location = new System.Drawing.Point(291, 240);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 46);
+            this.button1.Size = new System.Drawing.Size(80, 37);
             this.button1.TabIndex = 5;
             this.button1.Text = "Register";
             this.button1.UseVisualStyleBackColor = true;
@@ -62,69 +64,77 @@
             // 
             // PasswordTextBox
             // 
-            this.PasswordTextBox.Location = new System.Drawing.Point(250, 146);
+            this.PasswordTextBox.Location = new System.Drawing.Point(188, 119);
+            this.PasswordTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PasswordTextBox.Name = "PasswordTextBox";
             this.PasswordTextBox.PasswordChar = '*';
-            this.PasswordTextBox.Size = new System.Drawing.Size(219, 22);
+            this.PasswordTextBox.Size = new System.Drawing.Size(165, 20);
             this.PasswordTextBox.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(182, 9);
+            this.label1.Location = new System.Drawing.Point(136, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(126, 36);
+            this.label1.Size = new System.Drawing.Size(104, 29);
             this.label1.TabIndex = 3;
             this.label1.Text = "Register";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(60, 105);
+            this.label2.Location = new System.Drawing.Point(45, 85);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.Size = new System.Drawing.Size(55, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Username";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(60, 151);
+            this.label3.Location = new System.Drawing.Point(45, 123);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Password";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(60, 196);
+            this.label4.Location = new System.Drawing.Point(45, 159);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(118, 17);
+            this.label4.Size = new System.Drawing.Size(90, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "Repeat password";
             // 
             // usernameTextBox
             // 
-            this.usernameTextBox.Location = new System.Drawing.Point(250, 100);
+            this.usernameTextBox.Location = new System.Drawing.Point(188, 81);
+            this.usernameTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(219, 22);
+            this.usernameTextBox.Size = new System.Drawing.Size(165, 20);
             this.usernameTextBox.TabIndex = 1;
             // 
             // RepeatPassword
             // 
-            this.RepeatPassword.Location = new System.Drawing.Point(250, 191);
+            this.RepeatPassword.Location = new System.Drawing.Point(188, 155);
+            this.RepeatPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.RepeatPassword.Name = "RepeatPassword";
             this.RepeatPassword.PasswordChar = '*';
-            this.RepeatPassword.Size = new System.Drawing.Size(219, 22);
+            this.RepeatPassword.Size = new System.Drawing.Size(165, 20);
             this.RepeatPassword.TabIndex = 3;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 296);
+            this.button2.Location = new System.Drawing.Point(9, 240);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(106, 46);
+            this.button2.Size = new System.Drawing.Size(80, 37);
             this.button2.TabIndex = 6;
             this.button2.Text = "Back";
             this.button2.UseVisualStyleBackColor = true;
@@ -132,10 +142,10 @@
             // 
             // Register
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::holy_water.Properties.Resources.dribbble;
-            this.ClientSize = new System.Drawing.Size(506, 354);
+            this.ClientSize = new System.Drawing.Size(380, 288);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.RepeatPassword);
             this.Controls.Add(this.usernameTextBox);
@@ -146,9 +156,11 @@
             this.Controls.Add(this.PasswordTextBox);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox18);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Register";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Register";
+            this.Load += new System.EventHandler(this.Register_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
